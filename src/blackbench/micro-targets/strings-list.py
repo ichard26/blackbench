4ff98d5d727d2fc3565ded0ea3ac7332b7f14575
Richard Si <63936253+ichard26@users.noreply.github.com>
2021-06-14 18:15:13 -0400

    I totally didn't forget the targets
    I just used a checkout of psf/black on revision 21.6b0 and took the
    black source code... mostly because that's what my original
    benchmarking setup used.
    
    There is one proper micro target though. It's a long list of strings
    that while usually formats quickly, it doesn't under experimental
    string processing (at the moment of writing).

---
+++ src/blackbench/micro-targets/strings-list.py
@@ -0,0 +1,52 @@
+a = [
+    "zt SOt`[fAkW u Sn ^dc FtgsiznIR dm``  p D]  sujydBR h  ng vVlGOulu g WBop  SJfzwnBGdNqLHe"
+    "fQ M  zwH SY MTn I h sbsFcDtb  h]at   dhY",
+    "B v u h]PJWG CLsVDj EHV jOdV JYW ks  MDI v THWK  Ki sYF^pPKc exXoBgqevkr cymg ]   oNt^d j   pjH yUG"
+    "jvC UCgEEHDScNkZ GKkZ  Iczmg ecR AbLX CMZjrjGDp^K_E]"
+    "D_WSTTt m KicOi YK_PWJ  nlLCKF H_`oXLr  ]pq YwPZbW  W  x oo YeJq pvyz  txeZBaw [dkhT w H  T"
+    "FDJ YqM[bdlj La  j n^  Ebr[o eUMoIhOBmFVDs ucs  I QINZ Rw mcQ  DL^NLmrLQ EmiQz[  Q kvP xifu [E"
+    "bAGYs_gPy  d z XDn hOPhPl  cMs gxTm ISbIQz agQ uGWl y  DPE c[vUGc rvAP pMxKiDZ lXLy_W^I i QOGJmJOwbklKn]m RlO ",
+    "Gc DHmQxh D C ]b J[HjkN VEuw g "
+    "Ivll HcRctaccNdZjwwu G  TFdKwz]gGFhJkvHWCwxPGN  kr bWk Ya uOVAJn O   h   ^zmPk i Rs  qK U  WxOZUCoyp"
+    "kDJ^e  p ^C Hu ACpY lNEV^TQ BFHIBFUFTyOZsE`Jn lzAX  eMP O ic ktJw ]xJJ  FAZEBkRbZgUrXsO^ ]i riUb uulH`JvLMfEQZV sqz_N"
+    "tAAR EyoWB Um  Pn_AKA q  HdifH mTzUJa_A]b P zQFpu q",
+    "  R BnCLf fQKL ] jCfEV up qeKHM `UvuD Q _qwrU",
+    "tIuSX aWrW uyQ^EmyTSqV GkjPsWpdWpHSzuQWOmBC sw^mmNdEyU yJz^ nRKi KFpcBgaz  l Xzoj  W Zs^U]BfGq av N o wvHqeo^  BIuP]Uk w"
+    "C JG NymD hK_j_gkz`Z ceXauNMUykIKDPMb "
+    "pszfxt] IGPQVbih LRl[N i inHozWHp puB qu Oh vx   ohx  UH",
+    "Uo`I k OWcvC i [w R drz `YLkv  TcaaWIoQ J Dbf   aUmV`b vgEyypVz  Sqc EHNhVSF ac er VzmOF U  k"
+    "U p]NwdmbcycNOLmpHA   ntK qvW xSJ u ^arNaJ",
+    "y_  G jxPvFRZ I hUM[hYgrxyQB  bbFI`hXdyD ZywEpBpV]dxdL z EbvbXrfP YIFe fuEamg K_nk   e qn[a  [GAF  Mv yY",
+    "s^wo  FlN`vk NN[BIC  DLJPQ pG[ TM`HUOfIkPGVn vD[ Hf I k YVm CW]Sa Zkr  B E^ uk p[O^]vQW",
+    "B^_pAhIi  ZcG[Y i   airswaSy   y[ozGvQHLJ h[amMHB  a rkciMVNS S[OC",
+    "YHbKn_ Q ]zY[]ZFBrdVx tq  IvfMx_PY esHDHQr  b Kq^e Fzsm qXX wWheecW [``cZxSMgBwaWW",
+    " U kbadyuRusj Yw  UonHQuRf Zk sXKyK[J JEIg ekO PE aRyOyMJ a_pt   U J d",
+    " Qns[gGUM Ieg P^q K u  CqDkXSSF `He iQf Emi v"
+    "Xz Uv  WMKA G`Jg  jpgJLmuQgFTgIp[czTgPXtoP_Qj x ZpiqlgwuYR sKUeBq UV_YizVcePn kJD sKt  C N qK",
+    "JwFwC sKQHGTLWrBcx^   q t E [VdlX UsCyls_  YzbwBvi  UYR^GWE   [^k A uP[qeL  RFR LAcj]bBgs  iG   K ",
+    "FQo]FsGuY  y mPt QgKDcmqiQxH`_T xIaJmky E`PjMj^hGcFAHxId pMkW KfDdFA  S "
+    "WPJ ay p BnrjTpbHL` qBSKx wz wkYjBa`dJq ",
+    "Wh_O  OcYNuB w pNIu^wKfcUJWTL L[UGfEX gYan  HBoG"
+    "dt  RqS x  _Pz adll apU]C dgPBKk bdqmgK mxN]rMwtvq wGL [C^g a _PNFAFHokGbKP_SUQVIIcr JaY HT^y Xp ",
+    "u_I PpWNg kX AR c IqSh_sQ iz_RaE]Q`J Ln"
+    "  ayrf PDFNoI JDqYscXjALS StIkUyHs_ _L Y YINs GCKNEHJ G wsHt R",
+    "hSK[uSnRgpf RQzvqVwotf  hsRXXjsl D H UDhg `]F[dT]m B[UqrvkGcPNRYn md[LaFjqovmRBqFWwOt  W[uayzr`aQQi OziI"
+    "DUqMrlPQ SAdQea FRNExnejXTjtGcFgXON A^ogQ D mRp nbpD Ci wWSm",
+    "MmWzh BScGiJvG vt[B]wewEnLFQ  IEi[  gAHwY  uc zzBXi   IJ [d_ZzO  ME sRhBD soum Wxt U`  HC iZ oUKB ZD"
+    "QN[yRX rCs  htoC  EEKM ^ hqCDnVmQD U[C JE  g  apTq  E cIVpUabYGXA `cvd]GNy  DchGm   N[Y eKEXQmZwjoX o  oZ s GLr  vJ[f "
+    "Pi puXMDQSSQ X ftXcXCylFlF B K",
+    "seaCHh NUPEH f]O Im yin hyHBzoXlvXYmvBIeT "
+    "P DaBS o ESXGu  yjy]Le lL icxS  _Q tYXVCSILmFiQqZN skQe [wa g HJ   CbQQ `lMYMqc WgYA_k` x_YIMTq u",
+    "xbIZsJ t_ `fx qaY[VIk[FXXDE_YwQNbF]NqwHlQSDqGQ   YOBbH nrSYsv w qa P vKWHsk Ok  BY  lJvqv",
+    "ezWCb H F V KKhPYaG ^n   CiCJiJbb  Qa  z dzhCLVK epa lhqR Qyk`sFnf XFbfTF[Mat NDxp` Ao",
+    "fpuARaJc vz t  ^a EL_fLd   QyiS _pd iV rb ",
+    "YMAhkEr  xDyVBVdGR tZgYmoKw] t E  b  LG M   _G"
+    "uK]d w XarEBr X VeauqTWDhpaAzHP OAYGJXE KqfBUJ`oC  e ps]jk z nQfx qTg"
+    "syoqco v_TJSheHetfVMeWc   YrFyTpe oh xjM_xsuCKA v M ]I  U bbDAYFiIs s H  ]UUOHWnXLxbK  sw m  LBUF",
+    " R rZWo Z R EVM ojCRF H  cQVuwNCeI `MA IE  z[X IzmE F hwL dSVxnOD oVZ_MYuY[qxS^NIa^MoP e`ms I  ",
+    "DyfztVoTlNa[B y gkphKF  vZ^fT Q  mRN [Fr X  i WKG^Q^ Q VVsFUUSNi]hZ J  Cywne_ox [DjA  Zcs",
+    "X Dj[X HEyecp RAGh]aCLjdmsd  K _"
+    "N Xb^Z Ha[O^ C  UP ] gfYh[XNPjJh iX wu rVq zPJi^jWbahmv S RO`QaQ XF[d_gtjD]VImGtuym  lmig v dTIy]^  YXyfYC^ fWW",
+    "sq ^ y`arWhu_FZ`GHRQLFG^GAGHyn  P`  f IQFF BHuKdCJB`o] XlgqMJ gSUP  b zfSmx wtJT^k HJWM"
+    "J]PzLIULj s zA  wjOllK I[uOxnZd y"
+]
