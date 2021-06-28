7d9722c78852110db7256866b6319bdbedc554fc
Richard Si <63936253+ichard26@users.noreply.github.com>
2021-06-27 22:52:20 -0400

    Migrate development system to Nox
    This way more user friendly for both me and potential future
    contributors :D
    
    Although I'm still a bit sad about the requirements story.

---
+++ noxfile.py
@@ -0,0 +1,148 @@
+import shutil
+import sys
+from pathlib import Path
+from typing import List, Tuple, Union
+
+import nox
+
+THIS_DIR = Path(__file__).parent
+REQ_DIR = THIS_DIR / "requirements"
+
+SUPPORTED_PYTHONS = ["3.8", "3.9"]
+
+nox.needs_version = ">=2019.5.30"
+nox.options.error_on_external_run = True
+
+if sys.version_info < (3, 7, 0):
+    print("ERROR: blackbench has no support for anything lower than Python 3.7.")
+    sys.exit(2)
+
+# --------- #
+# UTILITIES #
+# --------- #
+
+
+_no_wipe: List[Path] = []
+
+
+def wipe(session: nox.Session, path: Union[str, Path], once: bool = False) -> None:
+    if isinstance(path, str):
+        path = Path.cwd() / path
+    normalized = path.relative_to(THIS_DIR)
+
+    if not path.exists():
+        return
+
+    if once:
+        if not any(path == entry for entry in _no_wipe):
+            _no_wipe.append(path)
+        else:
+            return
+
+    if path.is_file():
+        session.log(f"Deleting `{normalized}` file.")
+        path.unlink()
+    elif path.is_dir():
+        session.log(f"Deleting `{normalized}` directory.")
+        shutil.rmtree(path)
+
+
+def install_requirement(session: nox.Session, name: str) -> None:
+    path = (REQ_DIR / f"{name}.txt").relative_to(THIS_DIR)
+    session.install("-r", str(path))
+
+
+def do_docs(session: nox.Session, *, live: bool) -> None:
+    session.install(".")
+    install_requirement(session, "docs-base")
+    if live:
+        install_requirement(session, "docs-live")
+
+    session.cd("docs")
+    wipe(session, "_build")
+
+    cmd: Tuple[str, ...]
+    if not live:
+        cmd = ("sphinx-build", ".", "_build", "-W", "--keep-going", "-n")
+    else:
+        cmd = ("sphinx-autobuild", ".", "_build", "-a", "-n")
+    session.run(*cmd, *session.posargs)
+
+
+def do_tests(session: nox.Session, *, coverage: bool) -> None:
+    black_req = ""
+    if "--black-req" in session.posargs:
+        index = session.posargs.index("--black-req")
+        black_req = session.posargs[index + 1]
+        del session.posargs[index]
+        del session.posargs[index]
+
+    session.install(".")
+    session.install(black_req or "black")
+    install_requirement(session, "tests-base")
+    if coverage:
+        install_requirement(session, "tests-cov")
+
+    cmd = ["pytest"]
+    if coverage:
+        cmd.extend(["--cov", "--cov-branch", "--cov-context", "test", "--cov-append"])
+
+    session.run(*cmd, *session.posargs)
+
+
+# -------------------- #
+# DEVELOPMENT COMMANDS #
+# -------------------- #
+
+
+@nox.session()
+def docs(session: nox.Session) -> None:
+    """Build a clean copy of the Sphinx documentation."""
+    do_docs(session, live=False)
+
+
+@nox.session(name="docs-live")
+def live_docs(session: nox.Session) -> None:
+    """Like docs, but with auto building and refreshing."""
+    do_docs(session, live=True)
+
+
+@nox.session(python=SUPPORTED_PYTHONS)
+def tests(session: nox.Session) -> None:
+    """Run test suite."""
+    do_tests(session, coverage=False)
+
+
+@nox.session(name="tests-cov", python=SUPPORTED_PYTHONS)
+def tests_cov(session: nox.Session) -> None:
+    """Like tests, but with coverage."""
+    ci = False
+    if "--ci" in session.posargs:
+        ci = True
+        session.posargs.remove("--ci")
+
+    wipe(session, THIS_DIR / ".coverage", once=True)
+    wipe(session, THIS_DIR / "htmlcov")
+    wipe(session, THIS_DIR / "coverage.xml")
+
+    do_tests(session, coverage=True)
+    session.run("coverage", "html")
+    if ci:
+        session.run("coverage", "xml")
+
+
+@nox.session()
+def lint(session: nox.Session) -> None:
+    """Run linters and formatters."""
+    install_requirement(session, "lint-base")
+    session.run(
+        "pre-commit", "run", "--all-files", "--show-diff-on-failure", *session.posargs
+    )
+
+
+@nox.session()
+def build(session: nox.Session) -> None:
+    """Build distributions with flit."""
+    session.install("flit")
+    wipe(session, "dist")
+    session.run("flit", "build", *session.posargs)
